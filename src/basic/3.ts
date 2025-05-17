f7c995f470f3f6c640e97ba4a1eaf957b6ee7c71
serhiilukianenkodev <99186322+serhiilukianenkodev@users.noreply.github.com>
2025-05-17 14:58:59 +0300

    start

---
+++ src/basic/3.ts
@@ -0,0 +1,2 @@
+const value: string | number = "prime";
+const isEnable: "enable" | "disable" = "enable";
---
+++ src/basic/5.ts
@@ -0,0 +1,15 @@
+enum DayOfWeek {
+  Monday = "Monday",
+  Tuesday = "Tuesday",
+  Wednesday = "Wednesday",
+  Thursday = "Thursday",
+  Friday = "Friday",
+  Saturday = "Saturday",
+  Sunday = "Sunday",
+}
+
+const isWeekend = (day: DayOfWeek) => {
+  return day === DayOfWeek.Sunday || day === DayOfWeek.Saturday;
+};
+
+isWeekend(DayOfWeek.Friday);
